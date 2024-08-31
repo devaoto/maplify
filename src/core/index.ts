08017259427077d25ff16992250dd154f4b9f963
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-08-31 09:25:42 +0600

    Core module

---
+++ src/core/index.ts
@@ -0,0 +1,8 @@
+export { Maplify, type Website } from "./mapper";
+export {
+  sanitizeTitle,
+  FindBestMatchByTitles,
+  type Title,
+  type Input,
+} from "./similarity";
+export * from "string-similarity";
